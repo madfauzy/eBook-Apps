eb056edb5a1eba1aa096fa8153532195a8c6d948
Ahmad Fauzy <madfauzy28@gmail.com>
2021-12-09 23:06:55 +0700

    Add: menambahkan file database sql

---
+++ ebookapps.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Nov 2021 pada 12.34
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ebookapps`
+--
+
+CREATE DATABASE `ebookapps`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ebooks`
+--
+
+CREATE TABLE `ebooks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `price` char(4) NOT NULL,
+  `cover` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `ebooks`
+--
+
+INSERT INTO `ebooks` (`id`, `title`, `author`, `category`, `price`, `cover`) VALUES
+(1, 'Tutorial Laravel', 'Tutorialspoint', 'Programming Languages', 'Paid', 'tutorial_laravel.jpg'),
+(2, 'Tutorial C++', 'Tutorialspoint', 'Programming Languages', 'Paid', 'tutorial_c++.jpg'),
+(3, 'Tutorial CSS', 'Tutorialspoint', 'Programming Languages', 'Paid', 'tutorial_css.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `ebooks`
+--
+ALTER TABLE `ebooks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `ebooks`
+--
+ALTER TABLE `ebooks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
