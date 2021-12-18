5bcf0b46fbfbf217b670c75a4d75b382b03ee402
Ahmad Fauzy <madfauzy28@gmail.com>
2021-12-19 01:39:48 +0700

    Fix: mengubah jquery menjadi native js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
-$(document).ready(function(){
-    $('.delete-ebook').on('click', function(){
-        let getLink = $(this).attr('href');
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = getLink;
-            }
-        })
-        return false;
+const deleteEbook = document.querySelector(".delete-ebook");
+deleteEbook.addEventListener('click',function(){
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = deleteEbook.getAttribute("href");
+        }
     })
+    return false;
 });
