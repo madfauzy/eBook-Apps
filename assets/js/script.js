02aff0f8c2baa6ce261a58499c07a70de83c145a
Ahmad Fauzy <madfauzy28@gmail.com>
2021-12-21 02:06:36 +0700

    Fix: memperbaiki bug ketika delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-const deleteEbook = document.querySelector(".delete-ebook");
+$(document).ready(function(){
+    $(".delete-ebook").on("click", function(){
+        let getLink = $(this).attr("href");
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = getLink;
+            }
+        })
+        return false;
+    })
+});
+
 const keyword = document.getElementById("keyword");
 const listEbook = document.querySelector("main");
-
-deleteEbook.addEventListener('click',function(){
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = deleteEbook.getAttribute("href");
-        }
-    })
-    return false;
-});
 
 keyword.addEventListener("keyup",function(){
     const xhr = new XMLHttpRequest();
--- index.php
+++ index.php
@@ -140,6 +140,7 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="assets/js/script.js"></script>
     <?php if(isset($_GET["delete"])) : ?>
