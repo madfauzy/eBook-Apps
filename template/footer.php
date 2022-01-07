b74788ec6a737a9591b6eabfe99587636748e071
Ahmad Fauzy <madfauzy28@gmail.com>
2022-01-07 13:58:23 +0700

    Fix: delete template

--- template/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-dark text-light p-sm-4 py-4 px-0">
-    <div class="container d-flex flex-column flex-lg-row justify-content-between align-items-center">
-        <div>&copy; 2022 Copyright <a class="link-warning text-decoration-none fw-bold" href="https://github.com/madfauzy" target="_blank" rel="noopener">Ahmad Fauzy</a>. All Rights Reserved.</div>
-        <div>Icon made by <a class="link-warning text-decoration-none fw-bold" href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank" rel="noopener">Freepik</a> from <a class="link-warning text-decoration-none fw-bold" href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener">www.flaticon.com</a></div>
-    </div>
-</footer>
